7f3a2961c66a775370bd6ca9f1bb05bb7e093789
Karini Govender <karini@vivosa.co.za>
2023-02-03 13:18:35 +0200

    NAL-383: Linting

--- tests/test_insert_metadata.py
+++ tests/test_insert_metadata.py
@@ -1,18 +1,24 @@
+"""Module to test insertMetadata.py"""
+
 import json
 
 from ska_sdp_data_product_api.api.insert_metadata import InsertMetadata
 
-class Indices:
+
+class MockIndices:
     """Mocked Indices."""
-    
+
     def __init__(self, values):
+        """Init the fake Indices."""
         self.values = values
 
-    def create(self, index, ignore, body):
-        self.values[index] = {'schema': body}
-    
+    def create(self, index, ignore, body):  # pylint: disable=W0613
+        """Create Index."""
+        self.values[index] = {"schema": body}
+
     def get(self, index):
-        return self.values[index]['schema']
+        """Retrive Index."""
+        return self.values[index]["schema"]
 
 
 class MockElasticsearch:
@@ -22,47 +28,56 @@ class MockElasticsearch:
         """Init the fake Elasticsearch."""
         self.values = {}
         self.count = 1
-        self.indices = Indices(self.values)
-
+        self.indices = MockIndices(self.values)
 
     def index(self, index, document):
         """Set a value."""
-        if index not in self.values.keys():
-            self.values[index] = {'schema': json.dumps(document)}
+        if index not in self.values:
+            self.values[index] = {"schema": json.dumps(document)}
         self.values[index][self.count] = json.loads(document)
-        self.count+=1
-        
+        self.count += 1
 
-    def get(self, index, id):
+    def get(self, index, id):  # pylint: disable=W0622,C0103
         """Get a value or None."""
         return self.values[index][id]
 
 
-############################################################################################################
+#############################################################################
+
 
 def test_create_schema():
-    insertMetadata = InsertMetadata()
-    insertMetadata.es_client = MockElasticsearch()
+    """Method to test creation of schema."""
+    insert_metadata = InsertMetadata()
+    insert_metadata.es_client = MockElasticsearch()
 
-    with open('tests/test_files/example_files/example_schema.json', 'r') as schema_file:
+    with open(
+        "tests/test_files/example_files/example_schema.json",
+        "r",
+        encoding="UTF-8",
+    ) as schema_file:
         schema = schema_file.read()
-    
-    insertMetadata.create_index_from_schema("example_index", schema)
 
-    response = insertMetadata.es_client.indices.get("example_index")
+    insert_metadata.create_index_from_schema("example_index", schema)
+
+    response = insert_metadata.es_client.indices.get(index="example_index")
 
     assert response == schema
 
 
 def test_insert_metadata():
-    insertMetadata = InsertMetadata()
-    insertMetadata.es_client = MockElasticsearch()
+    """Method to test insertion of metadata."""
+    insert_metadata = InsertMetadata()
+    insert_metadata.es_client = MockElasticsearch()
 
-    with open('tests/test_files/example_files/example_metadata.json', 'r') as document_file:
+    with open(
+        "tests/test_files/example_files/example_metadata.json",
+        "r",
+        encoding="UTF-8",
+    ) as document_file:
         document = document_file.read()
 
-    insertMetadata.insert_metadata("example_index", document)
+    insert_metadata.insert_metadata("example_index", document)
 
-    response = insertMetadata.retrieve_metadata("example_index", 1)
+    response = insert_metadata.retrieve_metadata("example_index", 1)
 
     assert response == document
