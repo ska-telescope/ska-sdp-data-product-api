e34276359a1b45145c52f92899054c0ae431746e
Christian Christelis <cchristelis@ska.ac.za>
2023-08-21 16:12:25 +0200

    NAL-712 adding time to repeated re-indexing request as well as some refactoring.

--- src/ska_sdp_dataproduct_api/main.py
+++ src/ska_sdp_dataproduct_api/main.py
@@ -10,9 +10,7 @@ from ska_sdp_dataproduct_api.core.helperfunctions import (
     DPDAPIStatus,
     FileUrl,
     SearchParametersClass,
-    download_file,
-    ingest_metadata_files,
-    load_metadata_file,
+    download_file
 )
 from ska_sdp_dataproduct_api.core.settings import ES_HOST, app
 from ska_sdp_dataproduct_api.elasticsearch.elasticsearch_api import (
@@ -21,26 +19,19 @@ from ska_sdp_dataproduct_api.elasticsearch.elasticsearch_api import (
 from ska_sdp_dataproduct_api.inmemorystore.inmemorystore import (
     InMemoryDataproductIndex,
 )
-
+from ska_sdp_dataproduct_api.metadatastore.datastore import Store
 logger = logging.getLogger(__name__)
 
 DPD_API_Status = DPDAPIStatus()
 
-elasticsearch_metadata_store = ElasticsearchMetadataStore()
-elasticsearch_metadata_store.connect(hosts=ES_HOST)
-
-in_memory_metadata_store = InMemoryDataproductIndex(
-    elasticsearch_metadata_store.es_search_enabled,
-)
+store = Store.specialise(ES_HOST)
 
 
 @app.get("/status")
 async def root():
     """An enpoint that just returns confirmation that the
     application is running"""
-    return DPD_API_Status.status(
-        elasticsearch_metadata_store.es_search_enabled
-    )
+    return DPD_API_Status.status(store.es_search_enabled)
 
 
 @app.get("/reindexdataproducts")
@@ -48,13 +39,9 @@ async def reindex_data_products(background_tasks: BackgroundTasks):
     """This endpoint clears the list of data products from memory and
     re-ingest the metadata of all data products found"""
     DPD_API_Status.update_data_store_date_modified()
-    if elasticsearch_metadata_store.es_search_enabled:
-        store = elasticsearch_metadata_store
-    else:
-        store = in_memory_metadata_store
     background_tasks.add_task(store.reindex)
     logger.info("Metadata store cleared and re-indexed")
-    return "Metadata store cleared and re-indexed"
+    return "Metadata is set to be cleared and re-indexed"
 
 
 @app.post("/dataproductsearch", response_class=Response)
@@ -62,13 +49,11 @@ async def data_products_search(search_parameters: SearchParametersClass):
     """This API endpoint returns a list of all the data products
     in the PERSISTANT_STORAGE_PATH
     """
-    if not elasticsearch_metadata_store.es_search_enabled:
-        elasticsearch_metadata_store.connect(hosts=ES_HOST)
-        if not elasticsearch_metadata_store.es_search_enabled:
-            raise HTTPException(
-                status_code=503, detail="Elasticsearch not found"
-            )
-    filtered_data_product_list = elasticsearch_metadata_store.search_metadata(
+    if not store.es_search_enabled:
+        raise HTTPException(
+            status_code=503, detail="Elasticsearch not found"
+        )
+    filtered_data_product_list = store.search_metadata(
         start_date=search_parameters.start_date,
         end_date=search_parameters.end_date,
         metadata_key=search_parameters.key_pair.split(":")[0],
@@ -82,7 +67,7 @@ async def data_products_list():
     """This API endpoint returns a list of all the data products
     in the PERSISTANT_STORAGE_PATH
     """
-    return json.dumps(in_memory_metadata_store.metadata_list)
+    return json.dumps(store.metadata_list)
 
 
 @app.post("/download")
@@ -96,7 +81,7 @@ async def download(file_object: FileUrl):
 async def data_product_metadata(file_object: FileUrl):
     """This API endpoint returns the data products metadata in json format of
     a specified data product."""
-    return load_metadata_file(file_object)
+    return store.load_metadata_file(file_object)
 
 
 @app.post("/ingestnewdataproduct")
@@ -104,14 +89,7 @@ async def ingest_new_data_product(file_object: FileUrl):
     """This API endpoint returns the data products metadata in json format of
     a specified data product."""
     DPD_API_Status.update_data_store_date_modified()
-    if elasticsearch_metadata_store.es_search_enabled:
-        ingest_metadata_files(
-            elasticsearch_metadata_store, file_object.fullPathName
-        )
-    else:
-        ingest_metadata_files(
-            in_memory_metadata_store, file_object.fullPathName
-        )
+    store.ingest_metadata_files(file_object.fullPathName)
     logger.info(
         "New data product metadata file loaded and store index updated"
     )
