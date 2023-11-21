5b7082054ff5a7759b2ee5bd7db56b9d426d6533
Christian Christelis <cchristelis@ska.ac.za>
2023-11-21 09:17:10 +0200

    NAL-827 updating default columns based on feedback.

--- src/ska_sdp_dataproduct_api/core/settings.py
+++ src/ska_sdp_dataproduct_api/core/settings.py
@@ -83,11 +83,11 @@ app.add_middleware(
 
 DEFAULT_DISPLAY_LAYOUT = [
     {"name": "execution_block", "width": 180},
-    {"name": "id", "width": 20},
     {"name": "date_created", "width": 60},
     {"name": "observer", "width": 60},
-    {"name": "execution_block", "width": 120},
     {"name": "processing_block", "width": 180},
     {"name": "Intent", "width": 180},
     {"name": "notes", "width": 180},
+    {"name": "file_size", "width": 60},
+    {"name": "status", "width": 60},
 ]
