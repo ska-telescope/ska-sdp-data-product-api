0394a3b6b993d21fe32ba49aad8232cb40ccdc7f
Andre Odendaal <andre@vivosa.co.za>
2023-11-29 09:46:15 +0200

    REL-1092: bump-patch-release v0.6.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -22,9 +22,9 @@ copyright = '2022, SKA Organisation'
 author = ''
 
 # The short X.Y version
-version = '0.6.0'
+version = '0.6.1'
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '0.6.1'
 
 
 # -- General configuration ---------------------------------------------------
