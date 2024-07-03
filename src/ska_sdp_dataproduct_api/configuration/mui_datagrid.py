e12f56a3033c0c4195339f75dd711376e82880d9
Andre Odendaal <andre@vivosa.co.za>
2024-07-03 10:09:17 +0200

    NAL-1093: Cleanup of unused code in MuiDataGrid class

--- src/ska_sdp_dataproduct_api/configuration/mui_datagrid.py
+++ src/ska_sdp_dataproduct_api/configuration/mui_datagrid.py
@@ -38,28 +38,27 @@ class MuiDataGrid:
         self.initial_column_config: list[dict] = [
             {
                 "field": "execution_block",
-                "headerName": "execution_block",
+                "headerName": "Execution Block",
                 "width": 250,
                 "hide": False,
             },
-            {"field": "date_created", "headerName": "date_created", "width": 150, "hide": False},
-            {"field": "context.observer", "headerName": "observer", "width": 150, "hide": False},
+            {"field": "date_created", "headerName": "Date Created", "width": 150, "hide": False},
+            {"field": "context.observer", "headerName": "Observer", "width": 150, "hide": False},
             {
                 "field": "config.processing_block",
-                "headerName": "processing_block",
+                "headerName": "Processing Block",
                 "width": 250,
                 "hide": False,
             },
             {"field": "context.intent", "headerName": "Intent", "width": 300, "hide": False},
-            {"field": "context.notes", "headerName": "notes", "width": 500, "hide": False},
-            {"field": "size", "headerName": "file_size", "width": 80, "hide": False},
-            {"field": "status", "headerName": "status", "width": 80, "hide": False},
+            {"field": "context.notes", "headerName": "Notes", "width": 500, "hide": False},
+            {"field": "size", "headerName": "File size", "width": 80, "hide": False},
+            {"field": "status", "headerName": "Status", "width": 80, "hide": False},
         ]
 
-        self.columns: list[dict] = [
-            {"field": "id", "hide": True},
-        ]
+        self.columns: list[dict] = []
 
+        # This is to be used to programatically add new columns, method still to be created.
         self.columns_with_default_col_def: list[dict] = [
             {
                 "width": 100,
@@ -92,54 +91,12 @@ class MuiDataGrid:
         self.initial_state: dict = {"columns": {"columnVisibilityModel": {"id": False}}}
 
         for item in self.initial_column_config:
-            existing_options = self.get_column_keys(item)
-            self.columns.append(existing_options)
-            column_with_default_col_def = self.get_default_col_def(**item)
-            self.columns_with_default_col_def.append(column_with_default_col_def)
+            self.columns.append(item)
 
         self.table_config: dict = {}
         self.table_config["columns"] = self.columns
-        self.table_config["columnsWithDefaultColDef"] = self.columns_with_default_col_def
-        self.table_config["initialState"] = self.initial_state
 
         self.rows: list[dict] = []
-
-    def get_default_col_def(self, **kwargs) -> dict:
-        """
-        Returns a column definition dictionary with defaults filled and
-        specified values merged.
-
-        Args:
-            **kwargs: Keyword arguments representing column properties to override defaults.
-
-        Returns:
-            A dictionary containing all column properties.
-        """
-
-        # Start with a copy of the default column
-        column = self.default_column.copy()
-
-        # Update the column with specified keyword arguments
-        column.update(kwargs)
-
-        return column
-
-    def get_column_keys(self, column_options: dict) -> dict:
-        """
-        Returns a dictionary containing all keys from column_options that exist in the
-        default_columns.
-
-        Args:
-            column_options (dict): A dictionary containing potential column options.
-
-        Returns:
-            dict: A dictionary containing only the keys from column_options that are present in
-            the default_columns.
-        """
-        existing_keys = {
-            key: value for key, value in column_options.items() if key in self.default_column
-        }
-        return existing_keys
 
     def add_datagrid_row(self, row: dict) -> None:
         """Adds a dict of data to the datagrid row object.
