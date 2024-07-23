7d957cf83eb5d8754b07688efafc9af05291510c
Andre Odendaal <andre@vivosa.co.za>
2024-07-23 23:34:34 +0200

    NAL-1128: PosgreSQL methods refactor

--- src/ska_sdp_dataproduct_api/components/postgresql/postgresql.py
+++ src/ska_sdp_dataproduct_api/components/postgresql/postgresql.py
@@ -5,6 +5,7 @@ import json
 import logging
 
 import psycopg
+from psycopg.errors import OperationalError
 
 from ska_sdp_dataproduct_api.configuration.settings import (
     POSTGRESQL_HOST,
@@ -14,62 +15,44 @@ from ska_sdp_dataproduct_api.configuration.settings import (
     POSTGRESQL_USER,
 )
 
-logger = logging.getLogger(__name__)
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-arguments
 
 
-class PostgresConnector:  # pylint: disable=too-many-instance-attributes
+class PostgresConnector:
     """
     A class to connect to a PostgreSQL instance and test its availability.
-    A class to connect to a PostgreSQL instance and t
     """
 
-    def __init__(self):
-        self.host: str = POSTGRESQL_HOST
-        self.port: int = POSTGRESQL_PORT
-        self.user: str = POSTGRESQL_USER
-        self.password: str = POSTGRESQL_PASSWORD
+    def __init__(self, host: str, port: int, user: str, password: str, table_name: str):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.table_name = table_name
         self.conn = None
+        self.logger = logging.getLogger(__name__)
         self.postgresql_running: bool = False
-        self.postgresql_version: str = ""
-        self.connection_established_at = ""
-        self.connection_error = ""
-
-        self.connect()
+        self._connect()
         self.create_metadata_table()
 
     def status(self) -> dict:
         """
         Retrieves the current status of the PostgreSQL connection.
 
-        This method returns a dictionary containing the following information:
-
-        * `host`: The hostname or IP address of the PostgreSQL instance.
-        * `port`: The port number on which PostgreSQL is listening.
-        * `database`: The name of the database being connected to.
-        * `user`: The username used for authentication with PostgreSQL.
-        * `running` (optional): A boolean indicating whether PostgreSQL is currently running
-        (if available).
-        * `postgresql_version` (optional): The version of PostgreSQL being used (if applicable).
-        * `connection_established_at` (optional): A timestamp representing when a connection was
-        last established with PostgreSQL. This value might not be available depending on the
-        database library used.
-        * `connection_error` (optional): An error object containing details about any connection
-        errors that occurred (if applicable).
-
         Returns:
             A dictionary containing the current status information.
         """
-
         return {
             "host": self.host,
             "port": self.port,
             "user": self.user,
             "running": self.postgresql_running,
-            "postgresql_version": self.postgresql_version,  # Optional
-            "connection_error": self.connection_error,  # Optional
+            "table_name": self.table_name,
+            "postgresql_version": self._get_postgresql_version(),
         }
 
-    def connect(self) -> None:
+    def _connect(self) -> None:
         """
         Attempts to connect to the PostgreSQL instance.
 
@@ -78,25 +61,24 @@ class PostgresConnector:  # pylint: disable=too-many-instance-attributes
         """
         try:
             self.conn = psycopg.connect(
-                host=self.host,
-                port=self.port,
-                user=self.user,
-                password=self.password,
+                host=self.host, port=self.port, user=self.user, password=self.password
             )
             self.postgresql_running = True
-
-            # Attempt to retrieve PostgreSQL version if connected
-            with self.conn.cursor() as cursor:
-                cursor.execute("SELECT version()")
-                self.postgresql_version = cursor.fetchone()[0]
-
-        except (Exception, psycopg.Error):  # pylint: disable=broad-exception-caught
-            self.conn = None
+            self.logger.info("Connected to PostgreSQL successfully")
+        except OperationalError as error:
             self.postgresql_running = False
-            logger.error("PostgreSQL connection to %s:%s failed", str(self.host), str(self.port))
+            self.logger.error(
+                "An error occurred while connecting to the PostgreSQL database: %s", error
+            )
+            raise
+
+    def _get_postgresql_version(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT version()")
+            return cursor.fetchone()[0]
 
     def create_metadata_table(self) -> None:
-        """Creates the metadata table named as defined in the env variable POSTGRESQL_TABLE_NAME
+        """Creates the metadata table named as defined in the env variable self.table_name
         if it doesn't exist.
 
         Raises:
@@ -104,23 +86,23 @@ class PostgresConnector:  # pylint: disable=too-many-instance-attributes
         """
 
         try:
-            logger.info("Creating PostgreSQL metadata table: %s", POSTGRESQL_TABLE_NAME)
+            self.logger.info("Creating PostgreSQL metadata table: %s", self.table_name)
 
-            cursor = self.conn.cursor()
             create_table_query = f"""
-                CREATE TABLE IF NOT EXISTS {POSTGRESQL_TABLE_NAME} (
+                CREATE TABLE IF NOT EXISTS {self.table_name} (
                     id SERIAL PRIMARY KEY,
                     data JSONB NOT NULL,
                     execution_block VARCHAR(255) DEFAULT NULL,
                     json_hash CHAR(64) UNIQUE
                 );
             """
+            cursor = self.conn.cursor()
             cursor.execute(create_table_query)
             cursor.close()
-            logger.info("PostgreSQL metadata table %s created.", POSTGRESQL_TABLE_NAME)
+            self.logger.info("PostgreSQL metadata table %s created.", self.table_name)
 
         except psycopg.Error as error:
-            logger.error("Error creating metadata table: %s", error)
+            self.logger.error("Error creating metadata table: %s", error)
             raise
 
     def calculate_metadata_hash(self, metadata_file_json: dict) -> str:
@@ -130,7 +112,7 @@ class PostgresConnector:  # pylint: disable=too-many-instance-attributes
     def check_metadata_exists_by_hash(self, json_hash: str) -> bool:
         """Checks if metadata exists based on the given hash."""
         cursor = self.conn.cursor()
-        check_query = f"SELECT EXISTS(SELECT 1 FROM {POSTGRESQL_TABLE_NAME} WHERE json_hash = %s)"
+        check_query = f"SELECT EXISTS(SELECT 1 FROM {self.table_name} WHERE json_hash = %s)"
         cursor.execute(check_query, (json_hash,))
         exists = cursor.fetchone()[0]
         cursor.close()
@@ -139,7 +121,7 @@ class PostgresConnector:  # pylint: disable=too-many-instance-attributes
     def check_metadata_exists_by_execution_block(self, execution_block: str) -> int:
         """Checks if metadata exists based on the given execution block."""
         cursor = self.conn.cursor()
-        check_query = f"SELECT id FROM {POSTGRESQL_TABLE_NAME} WHERE execution_block = %s"
+        check_query = f"SELECT id FROM {self.table_name} WHERE execution_block = %s"
         cursor.execute(check_query, (execution_block,))
         result = cursor.fetchone()
         cursor.close()
@@ -149,9 +131,7 @@ class PostgresConnector:  # pylint: disable=too-many-instance-attributes
         """Updates existing metadata with the given data and hash."""
         json_hash = self.calculate_metadata_hash(metadata_file_json)
         cursor = self.conn.cursor()
-        update_query = (
-            f"UPDATE {POSTGRESQL_TABLE_NAME} SET data = %s, json_hash = %s WHERE id = %s"
-        )
+        update_query = f"UPDATE {self.table_name} SET data = %s, json_hash = %s WHERE id = %s"
         cursor.execute(update_query, (metadata_file_json, json_hash, id_field))
         self.conn.commit()
         cursor.close()
@@ -160,7 +140,7 @@ class PostgresConnector:  # pylint: disable=too-many-instance-attributes
         """Inserts new metadata into the database."""
         json_hash = self.calculate_metadata_hash(metadata_file_json)
         cursor = self.conn.cursor()
-        insert_query = f"INSERT INTO {POSTGRESQL_TABLE_NAME} (data, json_hash, execution_block) \
+        insert_query = f"INSERT INTO {self.table_name} (data, json_hash, execution_block) \
 VALUES (%s, %s, %s)"
         cursor.execute(insert_query, (metadata_file_json, json_hash, execution_block))
         self.conn.commit()
@@ -174,22 +154,22 @@ VALUES (%s, %s, %s)"
             execution_block = metadata_file_dict["execution_block"]
 
             if self.check_metadata_exists_by_hash(json_hash):
-                logger.info("Metadata with hash %s already exists.", json_hash)
+                self.logger.info("Metadata with hash %s already exists.", json_hash)
                 return
 
             metadata_id = self.check_metadata_exists_by_execution_block(execution_block)
             if metadata_id:
                 self.update_metadata(metadata_file_json, metadata_id)
-                logger.info("Updated metadata with execution_block %s", execution_block)
+                self.logger.info("Updated metadata with execution_block %s", execution_block)
             else:
                 self.insert_metadata(metadata_file_json, execution_block)
-                logger.info("Inserted new metadata with execution_block %s", execution_block)
+                self.logger.info("Inserted new metadata with execution_block %s", execution_block)
 
         except psycopg.Error as error:
-            logger.error("Error saving metadata to PostgreSQL: %s", error)
+            self.logger.error("Error saving metadata to PostgreSQL: %s", error)
             raise
         except Exception as exception:  # pylint: disable=broad-exception-caught
-            logger.error("Error saving metadata to PostgreSQL: %s", exception)
+            self.logger.error("Error saving metadata to PostgreSQL: %s", exception)
 
     def count_jsonb_objects(self):
         """Counts the number of JSON objects within a JSONB column.
@@ -199,10 +179,10 @@ VALUES (%s, %s, %s)"
         """
 
         cursor = self.conn.cursor()
-        cursor.execute(f"SELECT COUNT(*) FROM {POSTGRESQL_TABLE_NAME}")
+        cursor.execute(f"SELECT COUNT(*) FROM {self.table_name}")
         result = cursor.fetchone()[0]
         cursor.close()
-        logger.info("Number of items in the DB: %s", result)
+        self.logger.info("Number of items in the DB: %s", result)
         return result
 
     def delete_postgres_table(self) -> bool:
@@ -216,22 +196,28 @@ VALUES (%s, %s, %s)"
         """
 
         try:
-            logger.info("PostgreSQL deleting database table %s", POSTGRESQL_TABLE_NAME)
+            self.logger.info("PostgreSQL deleting database table %s", self.table_name)
             cursor = self.conn.cursor()
-            cursor.execute(f"DROP TABLE IF EXISTS {POSTGRESQL_TABLE_NAME}")
+            cursor.execute(f"DROP TABLE IF EXISTS {self.table_name}")
             self.conn.commit()
             cursor.close()
-            logger.info("PostgreSQL database table %s deleted.", POSTGRESQL_TABLE_NAME)
+            self.logger.info("PostgreSQL database table %s deleted.", self.table_name)
             return True
 
         except psycopg.OperationalError as error:
-            logger.error(
+            self.logger.error(
                 "An error occurred while connecting to the PostgreSQL database: %s", error
             )
             return False
         except psycopg.ProgrammingError as error:
-            logger.error("An error occurred while executing the SQL statement: %s", error)
+            self.logger.error("An error occurred while executing the SQL statement: %s", error)
             return False
 
 
-persistent_metadata_store = PostgresConnector()
+persistent_metadata_store = PostgresConnector(
+    host=POSTGRESQL_HOST,
+    port=POSTGRESQL_PORT,
+    user=POSTGRESQL_USER,
+    password=POSTGRESQL_PASSWORD,
+    table_name=POSTGRESQL_TABLE_NAME,
+)
--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -3,7 +3,13 @@
 import pytest
 
 from ska_sdp_dataproduct_api.components.postgresql.postgresql import PostgresConnector
-from ska_sdp_dataproduct_api.configuration.settings import POSTGRESQL_TABLE_NAME
+from ska_sdp_dataproduct_api.configuration.settings import (
+    POSTGRESQL_HOST,
+    POSTGRESQL_PASSWORD,
+    POSTGRESQL_PORT,
+    POSTGRESQL_TABLE_NAME,
+    POSTGRESQL_USER,
+)
 
 # pylint: disable=duplicate-code
 
@@ -11,8 +17,14 @@ from ska_sdp_dataproduct_api.configuration.settings import POSTGRESQL_TABLE_NAME
 @pytest.fixture(autouse=True)
 def clean_database(mocker):
     """Mock connection logic for setup and teardown"""
-    inmemory_store_mocked = PostgresConnector()
-    mocker.patch.object(inmemory_store_mocked, "connect")
+    inmemory_store_mocked = PostgresConnector(
+        host=POSTGRESQL_HOST,
+        port=POSTGRESQL_PORT,
+        user=POSTGRESQL_USER,
+        password=POSTGRESQL_PASSWORD,
+        table_name=POSTGRESQL_TABLE_NAME,
+    )
+    mocker.patch.object(inmemory_store_mocked, "_connect")
     mocker.patch.object(inmemory_store_mocked, "conn")
     yield
 
@@ -20,7 +32,13 @@ def clean_database(mocker):
 def test_create_metadata_table(mocker):
     """Test for create_metadata_table"""
     # Mock cursor execution to avoid actual database interaction
-    inmemory_store_mocked = PostgresConnector()
+    inmemory_store_mocked = PostgresConnector(
+        host=POSTGRESQL_HOST,
+        port=POSTGRESQL_PORT,
+        user=POSTGRESQL_USER,
+        password=POSTGRESQL_PASSWORD,
+        table_name=POSTGRESQL_TABLE_NAME,
+    )
 
     mock_cursor = mocker.Mock()
     mocker.patch.object(inmemory_store_mocked, "conn").cursor.return_value = mock_cursor
@@ -44,7 +62,13 @@ def test_create_metadata_table(mocker):
 def test_delete_postgres_table(mocker):
     """Test for delete_postgres_table"""
     # Mock cursor execution to avoid actual database interaction
-    inmemory_store_mocked = PostgresConnector()
+    inmemory_store_mocked = PostgresConnector(
+        host=POSTGRESQL_HOST,
+        port=POSTGRESQL_PORT,
+        user=POSTGRESQL_USER,
+        password=POSTGRESQL_PASSWORD,
+        table_name=POSTGRESQL_TABLE_NAME,
+    )
 
     mock_cursor = mocker.Mock()
     mocker.patch.object(inmemory_store_mocked, "conn").cursor.return_value = mock_cursor
