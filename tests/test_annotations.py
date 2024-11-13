e2b8cb416447810a8c62f49eeadd0bd64154aee5
karini1 <karini@vivosa.co.za>
2024-11-13 15:53:21 +0200

    NAL-1295: Linting monster returns

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -2,7 +2,7 @@
 
 from ska_dataproduct_api.components.annotations.annotation import DataProductAnnotation
 
-example_annotation = '{ \
+EXAMPLE_ANNOTATION = '{ \
     "data_product_uuid": "test_uuid", \
     "annotation_text": "test annotation", \
     "user_principal_name": "test user", \
@@ -12,13 +12,14 @@ example_annotation = '{ \
 
 
 def test_load_annotation_from_json():
-    dataProductAnnotation = DataProductAnnotation()
+    """Test to see if json string loads a class correctly."""
+    data_product_annotation = DataProductAnnotation()
 
-    dataProductAnnotation.load_annotation_from_json(example_annotation)
+    data_product_annotation.load_annotation_from_json(EXAMPLE_ANNOTATION)
 
-    assert dataProductAnnotation.annotation_id is None
-    assert dataProductAnnotation.data_product_uuid == "test_uuid"
-    assert dataProductAnnotation.annotation_text == "test annotation"
-    assert dataProductAnnotation.user_principal_name == "test user"
-    assert dataProductAnnotation.timestamp_created == "2024-11-13:14:32:00"
-    assert dataProductAnnotation.timestamp_modified == "2024-11-13:14:32:00"
+    assert data_product_annotation.annotation_id is None
+    assert data_product_annotation.data_product_uuid == "test_uuid"
+    assert data_product_annotation.annotation_text == "test annotation"
+    assert data_product_annotation.user_principal_name == "test user"
+    assert data_product_annotation.timestamp_created == "2024-11-13:14:32:00"
+    assert data_product_annotation.timestamp_modified == "2024-11-13:14:32:00"
